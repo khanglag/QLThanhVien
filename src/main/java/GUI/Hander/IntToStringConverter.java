2017abab51a59d1bf4658c7a86c48a4b6cb10c69
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-04-08 22:23:41 +0700

    bugsssss

---
+++ src/main/java/GUI/Hander/IntToStringConverter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI.Hander;
+
+/**
+ *
+ * @author khang
+ */
+public class IntToStringConverter {
+    public static String Converter(int n){
+        return String.format("%02d", n);
+    }
+}
