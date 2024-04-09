659747fcd36ef410ff8ad3716eea53a0c63a1044
kecuonq71 <kecuong71@gmail.com>
2024-04-09 15:26:53 +0700

    ...

--- src/main/java/BLL/test.java
+++ src/main/java/BLL/test.java
@@ -2,16 +2,17 @@ package BLL;
 
 import java.util.List;
 
+import DAL.ThanhVien;
 import DAL.ThietBi;
 
 public class test {
     public static void main(String[] args) {
-        ThietBiBLL bll = new ThietBiBLL();
-        ThietBi tb = new ThietBi(1000009, "Máy tính bảng", "Đây là máy tính bảng");
-        bll.addThietBi(tb);
-        List<ThietBi> list= bll.search("micro");
-        for( ThietBi b : list)
-            System.out.println(b.toString());
+        // ThietBiBLL bll = new ThietBiBLL();
+        // ThietBi tb = new ThietBi(1000009, "Máy tính bảng", "Đây là máy tính bảng");
+        // bll.addThietBi(tb);
+        // List<ThietBi> list= bll.search("micro");
+        // for( ThietBi b : list)
+        //     System.out.println(b.toString());
 
         // List<ThietBi> thietBiList = bll.loadThietbi();
         // Object[][] convertedData = bll.convertList(thietBiList);
@@ -21,5 +22,23 @@ public class test {
         //     }
         //     System.out.println();
         // }
+        // ThanhVienBLL bll= new ThanhVienBLL();
+        // ThanhVien newMember = new ThanhVien();
+        // newMember.setHoTen("Nguyen Van A");
+        // newMember.setKhoa("CNTT");
+        // newMember.setNganh("Cong nghe thong tin");
+        // newMember.setSDT(23456789);
+       
+        // bll.addThanhVien(newMember, 22,41);
+        // ThanhVienBLL a =new ThanhVienBLL();
+        // ThanhVien b = new ThanhVien();
+        // b.setMaTV(1120410002);
+        // b.setHoTen("A");
+        // b.setKhoa("QTKD");
+        // b.setNganh("CNTT");
+        // b.setSDT(85233);
+        // a.updateThanhVien(b);
+        
+
     }
 }
--- src/main/java/DAL/Test.java
+++ src/main/java/DAL/Test.java
@@ -9,19 +9,23 @@ public class Test {
     public static void main(String[] args) {
         try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             session.beginTransaction();
+            XuLy xuLy = new XuLy();
+        
+            // Thiết lập thông tin cho đối tượng XuLy
             ThanhVienDAL thanhVienDAL = new ThanhVienDAL();
-            ThanhVien newMember = new ThanhVien();
-            newMember.setHoTen("Nguyen Van A");
-            newMember.setKhoa("CNTT");
-            newMember.setNganh("Cong nghe thong tin");
-            newMember.setSDT(23456789);
+            ThanhVien thanhVien = thanhVienDAL.getThanhVien(1121530087); // Giả sử có một thành viên có mã là 1 trong cơ sở dữ liệu
+            xuLy.setMaTV(thanhVien);
+            xuLy.setHinhThucXL("Xử lý hình thức 1");
+            xuLy.setSoTien(1000);
+            xuLy.setNgayXL(LocalDateTime.now());
+            xuLy.setTrangThaiXL(1);
     
-           
+            // Tạo một đối tượng XuLyDAL
+            XuLyDAL xuLyDAL = new XuLyDAL();
     
-           
-            thanhVienDAL.addThanhVien(newMember, 22,41);
-           
-            // thanhVienDAL.deleteByActiveYear(18,41);
+            // Thêm đối tượng XuLy vào cơ sở dữ liệu
+            xuLyDAL.addXuLy(xuLy);
+    
 
 
             session.getTransaction().commit();
