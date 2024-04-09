57b0da505fb1eaac31156de9b2545eb110fcb71d
kecuonq71 <kecuong71@gmail.com>
2024-04-09 13:50:11 +0700

    sửa thành viên

---
+++ src/main/java/DAL/Main.java
@@ -0,0 +1,13 @@
+package DAL;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
