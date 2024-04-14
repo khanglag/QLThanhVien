27efdecfd154ee94f8053bdede1ca22f2fd7c042
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-04-14 15:20:20 +0700

    t

--- src/main/java/DAL/ThongTinSdDAL.java
+++ src/main/java/DAL/ThongTinSdDAL.java
@@ -4,6 +4,7 @@
  */
 package DAL;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.hibernate.Transaction;
@@ -57,16 +58,18 @@ public class ThongTinSdDAL {
         }
         return tt;
     }
-    //Tạo mã thông tin tự động
+
+    // Tạo mã thông tin tự động
     public int generateMaTT() {
         int newMaXL = 0;
         Integer maXL = (Integer) session.createQuery("SELECT MAX(MaTT) FROM ThongTinSD").uniqueResult();
-        if(maXL == null)
+        if (maXL == null)
             newMaXL = 1;
         else
-             newMaXL = maXL + 1;
+            newMaXL = maXL + 1;
         return newMaXL;
     }
+
     public void addThongTin(ThongTinSD tt) {
         Transaction transaction = null;
         try {
@@ -112,8 +115,112 @@ public class ThongTinSdDAL {
             if (transaction != null)
                 transaction.rollback();
             e.printStackTrace();
-        } finally {
-            session.close();
+        }
+    }
+
+    // Muợn thiết bị
+    public void borrowedDevice(int MaTV, int MaTB) {
+        Transaction transaction = null;
+        try {
+            openSession();
+            transaction = session.beginTransaction();
+
+            // Kiểm tra MaTV có trong bảng ThanhVien
+            ThanhVien thanhVien = session.get(ThanhVien.class, MaTV);
+            if (thanhVien == null)
+                throw new RuntimeException("Member does not exist");
+
+            // Kiểm tra thiết bị đã được cho mượn hay chưa
+            ThongTinSD thongTinSD = (ThongTinSD) session
+                    .createQuery("FROM ThongTinSD WHERE MaTB.id = :MaTB AND TGMuon IS NOT NULL AND TGTra IS NULL")
+                    .setParameter("MaTB", MaTB)
+                    .uniqueResult();
+            if (thongTinSD != null)
+                throw new RuntimeException("The device has been borrowed");
+
+            // Thiết bị chưa cho mượn
+            thongTinSD = (ThongTinSD) session.createQuery("FROM ThongTinSD WHERE MaTB.id = :MaTB AND TGTra IS NULL")
+                    .setParameter("MaTB", MaTB)
+                    .uniqueResult();
+            if (thongTinSD != null) {
+                thongTinSD.setTGMuon(LocalDateTime.now());
+                session.update(thongTinSD);
+                transaction.commit();
+                return;
+            }
+
+            thongTinSD = (ThongTinSD) session
+                    .createQuery("FROM ThongTinSD WHERE maTV = :maTV ORDER BY maTT DESC")
+                    .setParameter("MaTV", MaTV)
+                    .setMaxResults(1) // Chỉ lấy 1 kết quả (nếu có)
+                    .uniqueResult();
+            // Lấy thông tin của Thiết bị từ cơ sở dữ liệu
+            ThietBi thietBi = session.get(ThietBi.class, MaTB);
+            if (thongTinSD != null) {
+                // Cập nhật mã thiết bị
+                thongTinSD.setMaTB(thietBi);
+                // Sửa thời gian mượn
+                thongTinSD.setTGMuon(LocalDateTime.now());
+
+                // Lưu thay đổi vào cơ sở dữ liệu
+                session.update(thongTinSD);
+                transaction.commit();
+                System.out.println("Đã sửa thông tin sử dụng thành công.");
+            } else {
+                // Nếu thiết bị chưa có trong bảng ThongTinSD
+                ThongTinSD thongTinMuon = new ThongTinSD();
+                thongTinMuon.setMaTT(generateMaTT());
+                thongTinMuon.setMaTV(thanhVien);
+                thongTinMuon.setMaTB(thietBi);
+                thongTinMuon.setTGVao(LocalDateTime.now());
+                thongTinMuon.setTGMuon(LocalDateTime.now());
+                session.save(thongTinMuon);
+                transaction.commit();
+            }
+
+        } catch (HibernateException e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void suaThongTinSD(int maTB, int maTV) {
+        // Bắt đầu một transaction
+        Transaction transaction = null;
+        try {
+            openSession();
+            transaction = session.beginTransaction();
+
+            // Lấy thông tin của Thiết bị từ cơ sở dữ liệu
+            ThietBi thietBi = session.get(ThietBi.class, maTB);
+
+            // Tìm Thông tin sử dụng cần sửa dựa trên maTV và maTB
+            ThongTinSD thongTinSD = (ThongTinSD) session
+                    .createQuery("FROM ThongTinSD WHERE maTV = :maTV ORDER BY maTT DESC")
+                    .setParameter("maTV", maTV)
+                    .setMaxResults(1) // Chỉ lấy 1 kết quả (nếu có)
+                    .uniqueResult();
+
+            System.out.println(thongTinSD);
+            if (thongTinSD != null) {
+                // Cập nhật mã thiết bị
+                thongTinSD.setMaTB(thietBi);
+                // Sửa thời gian mượn
+                thongTinSD.setTGMuon(LocalDateTime.now());
+
+                // Lưu thay đổi vào cơ sở dữ liệu
+                session.update(thongTinSD);
+                transaction.commit();
+                System.out.println("Đã sửa thông tin sử dụng thành công.");
+            } else {
+                System.out.println("Không tìm thấy thông tin sử dụng để sửa.");
+            }
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 
--- src/main/java/DAL/t.java
+++ src/main/java/DAL/t.java
@@ -4,6 +4,7 @@
  */
 package DAL;
 
+import java.time.LocalDateTime;
 import org.hibernate.Session;
 
 /**
@@ -12,26 +13,51 @@ import org.hibernate.Session;
  */
 public class t {
     public static void main(String[] args) {
-            
-       try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
             session.beginTransaction();
             ThanhVienDAL thanhVienDAL = new ThanhVienDAL();
-            
+
             ThanhVien newMember = new ThanhVien();
             newMember.setHoTen("Nguyen Van A");
             newMember.setKhoa("CNTT");
             newMember.setNganh("Cong nghe thong tin");
             newMember.setSDT(23456789);
-           
-            thanhVienDAL.addThanhVien(newMember, 22,41);
-           
+
+            thanhVienDAL.addThanhVien(newMember, 22, 41);
+
             // thanhVienDAL.deleteByActiveYear(18,41);
 
+            // // ThanhVien newMember = new ThanhVien();
+            // // newMember.setHoTen("Nguyen Van A");
+            // // newMember.setKhoa("CNTT");
+            // // newMember.setNganh("Cong nghe thong tin");
+            // // newMember.setSDT(23456789);
 
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            // Xử lý ngoại lệ nếu có
-            e.printStackTrace();
+            // // thanhVienDAL.addThanhVien(newMember, 22,41);
+
+            // // thanhVienDAL.deleteByActiveYear(18,41);
+
+            // // XuLyDAL dal = new XuLyDAL();
+            // // List<XuLy> list= dal.loadXuLy();
+            // // for(XuLy xuLy : list)
+            // // System.out.println(xuLy);
+
+            // ThongTinSdDAL dal = new ThongTinSdDAL();
+            // // List<ThongTinSD> list= dal.loadThongTinSD();
+            // // for(ThongTinSD sd : list)
+            // // System.out.println(sd);
+            // // XuLyDAL dal = new XuLyDAL();
+            // // for(XuLy xl : dal.searchXuLy(1121530087))
+            // // System.out.println(xl);
+            // session.getTransaction().commit();
+            // } catch (Exception e) {
+            // // Xử lý ngoại lệ nếu có
+            // e.printStackTrace();
+            // }
+            ThongTinSdDAL dal = new ThongTinSdDAL();
+            // dal.suaThongTinSD(1000003, 1123330257);
+            dal.borrowedDevice(1123330257, 1000003);
         }
-}
+    }
 }
